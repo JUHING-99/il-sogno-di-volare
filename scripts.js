b112dc39c51deb90d10c3b0cb46a899b7b10fee8
Cristina Mateo <cris.mateo.99@gmail.com>
2023-11-28 14:24:22 +0100

    rellenar codigo

--- scripts.js
+++ scripts.js
@@ -35,17 +35,21 @@ const militaryHelicopter = [
 //Su única funcionalidad es albergar imágenes y devolverlas dependiendo del método que utilicemos. No interactúa con HTML.
 class Gallery {
     constructor(civilImages, militaryImages) {
+        this.civilImages = civilImages;
+        this.militaryImages = militaryImages;
     }
     
     // devuelve un vehículo aleatorio civil de la galería.
     getRandomCivil() {
-        Math.floor(Math.random() * 10)
+        return this.civilImages[Math.floor(Math.random() * this.civilImages.length)];
     }
     //devuelve un vehículo aleatorio militar de la galería.
     getRandomMilitary() {
+        return this.militaryImages[Math.floor(Math.random() * this.militaryImages.length)];
     }
     // devuelve el conjunto de vehículos de la galería, tanto militares como civiles.
     getAll() {
+        return [...this.civilImages, ...this.militaryImages];
     }
 }
 
@@ -57,10 +61,13 @@ class Painter {
 
     //Ejecutará la función createGallery
     constructor() {
+        this.createGallery();
     }
 
     //Creará un elemento section y lo agregará al body. Dicho section será, también, una propiedad de Painter a la que llamaremos gallery.
     createGallery() {
+        this.gallery = document.createElement('section');
+        document.body.appendChild(this.gallery);
     }
 
     //Acepta la url de una imagen y devuelve los siguientes elementos:
@@ -68,17 +75,28 @@ class Painter {
     //  <img src="ThisIsAnImage.jpg" />
     //</picture>
     createImageTag(imageUrl) {
+        const pictureElement = document.createElement('picture');
+        const imgElement = document.createElement('img');
+        imgElement.src = imageUrl;
+        pictureElement.appendChild(imgElement);
+        return pictureElement;
     }
 
     //Acepta la url de una imagen y agrega a gallery el elemento creado por createImageTag.
     paintSingleImage(imageUrl) {
+        const imageTag = this.createImageTag(imageUrl);
+        this.gallery.appendChild(imageTag);
     }
 
     //Acepta un conjunto de imágenes y agrega a gallery, uno a uno, el elemento creado por createImageTag.
     paintMultipleImages(arrayOfImages) {
+        arrayOfImages.forEach(imageUrl => {
+            this.paintSingleImage(imageUrl);
+        });
     }
 }
 
 const aircrafts = new Gallery(civilAircrafts, militaryAircrafts);
 const helicopters = new Gallery(civilHelicopters, militaryHelicopter);
 const painter = new Painter();
+
