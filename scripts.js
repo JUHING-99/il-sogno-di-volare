38b507a4119ca0e918f83baef7e57cf43c65dfea
Cristina Mateo <cris.mateo.99@gmail.com>
2023-11-28 14:02:03 +0100

    comentar funcionalidades

--- scripts.js
+++ scripts.js
@@ -30,33 +30,51 @@ const militaryHelicopter = [
     'assets/helicoptero-militar-5.jpg'
 ];
 
+
+
+//Su única funcionalidad es albergar imágenes y devolverlas dependiendo del método que utilicemos. No interactúa con HTML.
 class Gallery {
     constructor(civilImages, militaryImages) {
     }
     
+    // devuelve un vehículo aleatorio civil de la galería.
     getRandomCivil() {
+        Math.floor(Math.random() * 10)
     }
-    
+    //devuelve un vehículo aleatorio militar de la galería.
     getRandomMilitary() {
     }
-    
+    // devuelve el conjunto de vehículos de la galería, tanto militares como civiles.
     getAll() {
     }
 }
 
+
+
+//La clase encargada de pintar las imágenes, de interactuar con el DOM. Encargada de crear etiquetas y manipular el DOM para agregarlas.
 class Painter {
+
+
+    //Ejecutará la función createGallery
     constructor() {
     }
 
+    //Creará un elemento section y lo agregará al body. Dicho section será, también, una propiedad de Painter a la que llamaremos gallery.
     createGallery() {
     }
 
+    //Acepta la url de una imagen y devuelve los siguientes elementos:
+    //<picture>
+    //  <img src="ThisIsAnImage.jpg" />
+    //</picture>
     createImageTag(imageUrl) {
     }
 
+    //Acepta la url de una imagen y agrega a gallery el elemento creado por createImageTag.
     paintSingleImage(imageUrl) {
     }
 
+    //Acepta un conjunto de imágenes y agrega a gallery, uno a uno, el elemento creado por createImageTag.
     paintMultipleImages(arrayOfImages) {
     }
 }
